279908938510badfe5121ecaa7167f2e5e5a59c4
pabloatp0204 <pabloatp0204@gmail.com>
2014-08-12 20:26:50 -0500

    Intento de Obtener Internet

---
+++ Clase2/matriz.js
@@ -0,0 +1,17 @@
+// var menu = ["Productos", "Ventas", "Contacto"];
+// document.write( menu[0] );
+
+// var dofa = [["Fortaleza", "Oportunidad"], ["Debilidades", "Aminazas"]]
+// document.write( dofa[0][1] )
+
+function explosion()
+{
+	alert("BOOM!!");
+	document.write("<h1>BOOM! Elegiste una área minada :(</h1>");
+}
+
+function ganaste()
+{
+	document.write("Eres un ganador :)");
+}
+
